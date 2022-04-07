fbd2daa20b88bc71f452d06b5ade58a61a15b418
Methushelpathiwada <98147356+Methushelpathiwada@users.noreply.github.com>
2022-04-07 14:14:50 +0530

    Add files via upload

---
+++ fun.js
@@ -0,0 +1,72 @@
+
+// Create one function with zero parameter having a console statement;
+// function me(){
+//     console.log('methushel')
+// }
+// me()
+
+// // Create one function which takes two values as a parameter and print the sum of them as "Sum of 3, 4 is 7"
+// function sum(a,b){
+//     console.log(a+b)
+// }
+// sum(3,4)
+
+// // Create one arrow function
+// let arrow=()=>{
+//     console.log('akhil')
+// }
+// arrow()
+
+// "Print output: 
+// var x = 21;
+// var girl = function () {
+//     console.log(x);
+//     var x = 20;
+// };
+// girl ();
+
+// output:- 'undefined'
+
+// "Print output: 
+// var x = 21;
+// girl ();
+// console.log(x)
+// function girl() {
+//     console.log(x);
+//     var x = 20;
+// };
+
+// output:- undefined
+
+// "Print output
+
+// var x = 21;
+// a();
+// b();
+// console.log(a);
+// a = function() {
+    
+//    x = 20;
+//   console.log(x);
+// };
+// b = function() {
+    
+//     x = 40;
+//    console.log(x);
+// };
+
+// output:- 'reference error','a is not defined'
+
+// Write a function that accepts parameter n and returns factorial of n
+
+// function fact(n){
+//     if(n == 0 || n == 1){
+//         return 1;
+//     }else{
+//         return n * fact(n-1);
+//     }
+// }
+// result=fact(4);
+// console.log(result);
+
+// output:- 24
